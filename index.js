cb76b8c90ccaeace5997f9ce4a00bc76c5d23378
Jack Norris <jacktimothynorris@gmail.com>
2022-11-08 17:44:38 -0600

    Got basic twilio working

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+const accountSid = "**"
+const authToken = "**"
+import twilioClientCreator from 'twilio'
+
+
+const client = twilioClientCreator(accountSid, authToken)
+
+client.messages
+  .create({
+     body: 'Jarrett and Joseph say hi',
+     from: '+19452077377',
+     to: '+13343338745'
+   })
+  .then(message => console.log("I sent your message"));
+
+

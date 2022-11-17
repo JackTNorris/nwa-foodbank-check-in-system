33c7734744396b58aa73c6aab2c58c22860a52c4
Jack Norris <jacktimothynorris@gmail.com>
2022-11-16 19:45:46 -0600

    Added .env, adjusted comments

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const accountSid = "**"
-const authToken = "**"
+import dotenv from 'dotenv'
 import twilioClientCreator from 'twilio'
 
+dotenv.config();
 
-const client = twilioClientCreator(accountSid, authToken)
+const client = twilioClientCreator(process.env.SID, process.env.AUTH_TOKEN)
+
+const message = "Mason demo"
+
+const numbers = ['5555555555', '8888888888', '7777777777']
 
 client.messages
   .create({
-     body: 'Jarrett and Joseph say hi',
+     body: message,
      from: '+19452077377',
      to: '+13343338745'
    })
-  .then(message => console.log("I sent your message"));
+  .then(message => console.log("I sent your message"))
+  .catch(error => console.log(error.message))
 
 
